77057028644956c944b5256f4d9c6776d357989c
adeepak7494 <akhildeepak01@gmail.com>
2025-02-05 19:31:37 +0530

    removed tailwind and added material

--- NewsReader_UI/src/app/app.config.ts
+++ NewsReader_UI/src/app/app.config.ts
@@ -3,12 +3,13 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),provideHttpClient(),
 
     { provide: 'API_URL', useValue: 'https://newsapi.org/v2/' },
-    { provide: 'API_KEY', useValue:  'aa7437b64c0f4bf7873b4adda6dd7e48'}
+    { provide: 'API_KEY', useValue:  'aa7437b64c0f4bf7873b4adda6dd7e48'}, provideAnimationsAsync()
   ]
 
 
